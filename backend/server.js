a9416852353584f6216040507ee1f97919de99da
Arvid Sätterkvist <arvidsat@gmail.com>
2016-03-18 16:18:12 +0100

    connection to db

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ var express = require('express');        // call express
 var app = express();                 // define our app using express
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/test');
 
 var Bear = require('./models/bear');
 
